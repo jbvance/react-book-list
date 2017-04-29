b68537250069a55da856fabb6b45e94f602ef590
Jason Vance <jbvance@gmail.com>
2017-04-28 21:13:30 -0500

    Add activeBook reducer and action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
 
+//Wire the application state (key) to the reducers (value)
+//that way, when a reducer gets called, it only deals with the
+//relevant part of the application state
+//Any key provided to combineReducers ends up as a key on application state
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
